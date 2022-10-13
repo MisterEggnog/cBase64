595690fda70b0d87c60c6fbac392eeea22587d08
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-10-12 22:48:06 -0700

    Rewrote decoding function sig

--- CBase64Stream.h
+++ CBase64Stream.h
@@ -18,9 +18,13 @@ typedef struct {
 void
 base64_encode(const unsigned char input[], size_t len, char dest[]);
 
-// Convert 4 base64 numbers into data, return the number of decoded byres.
+// Convert 4 base64 numbers into 3 bytes, return the number of decoded byres.
+// input must be a string of at least 4 characters
 // Dest must be an array of size > 3.
+// return how many bytes were writen to dest
+//
+// Program may exit if input contains characters outside of BASE64_DIGITS or PADDING
 size_t
-base64_decode(const Base64Quard* data, unsigned char dest[]);
+base64_decode(const char input[], unsigned char dest[]);
 
 #endif // CBASE64STREAM_H_INCLUDED
