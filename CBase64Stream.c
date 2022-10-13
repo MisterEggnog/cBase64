8b2648077d8063dace1928a27f6a070d5265f011
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-10-12 23:02:35 -0700

    Removed all remaining references to Base64Quard

--- CBase64Stream.c
+++ CBase64Stream.c
@@ -1,23 +1,20 @@
 
 #include "CBase64Stream.h"
 
-static int size4_encode(const unsigned char* input, Base64Quard* dest);
+static void size4_encode(const unsigned char* input, char* dest);
 
 void
 base64_encode(const unsigned char input[], size_t len, char dest[]) {
-	int res;
 	switch (len) {
 		case 4:
 		case 3:
 		case 2:
 		case 1:
 		default:
-			res = 0;
+			break;
 	}
-	return res;
 }
 
-static int
-size4_encode(const unsigned char* input, Base64Quard* dest) {
-	return 0;
+static void
+size4_encode(const unsigned char* input, char* dest) {
 }
