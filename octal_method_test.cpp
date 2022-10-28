47eeb8fd876980fac5be08a5e1317910950ac61f
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-10-27 19:41:02 -0700

    Started writing octal test file

--- octal_method_test.cpp
+++ octal_method_test.cpp
@@ -1,1 +1,23 @@
-int main() {}
+#include "base64.h"
+#include <acutest.h>
+#include <random>
+
+void
+encoding_same_as_octal_method() {
+	auto rand = std::minstd_rand(7);
+	auto dist = std::uniform_int_distribution<char>(-128);
+
+	char input[4] = "";
+	unsigned char output[4];
+
+	for (int i = 0; i < 50; i++) {
+		input[0] = dist(rand);
+		input[1] = dist(rand);
+		input[2] = dist(rand);
+	}
+}
+
+TEST_LIST = {
+	{ "Library encoding provides same result as octal method", encoding_same_as_octal_method },
+	{ NULL, NULL },
+};
