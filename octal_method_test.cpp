0c90341c272c97ef22f5fb63fdad04b621b36957
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-10-27 20:41:56 -0700

    Wrote test for function to setup 24bit integer

--- octal_method_test.cpp
+++ octal_method_test.cpp
@@ -4,12 +4,15 @@
 #include <random>
 #include <iomanip>
 
+unsigned int
+get_octal_int(unsigned char input[]) {
+	 return (unsigned int)input[0] << 16 | (unsigned int)input[1] << 8 | (unsigned int)input[0];
+}
+
 // input must be size 4
 std::string
 get_octal_form(unsigned char input[]) {
-	auto twenty_fourbit = (unsigned int)input[0] << 16
-		| (unsigned int)input[1] << 8
-		| (unsigned int)input[0];
+	auto twenty_fourbit = get_octal_int(input);
 	auto string_builder = std::ostringstream();
 	string_builder << std::oct << std::setfill('0') << std::setw(8) << twenty_fourbit;
 	return string_builder.str();
@@ -39,7 +42,16 @@ get_octal_form_test() {
 	TEST_MSG("get_octal returned %s", result.c_str());
 }
 
+void
+get_octal_int_test() {
+	unsigned char input[] = { 0x12, 0x34, 0x56 };
+	auto result = get_octal_int(input);
+	TEST_CHECK(result == 0x123456);
+	TEST_MSG("get_octal_int returned %X", result);
+}
+
 TEST_LIST = {
+	{ "get octal int test", get_octal_int_test },
 	{ "get octal form test", get_octal_form_test },
 	{ "Library encoding provides same result as octal method", encoding_same_as_octal_method },
 	{ NULL, NULL },
