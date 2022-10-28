6aa594bdcea2c8fb5d12f0f6cd49365912fa3060
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-10-27 20:07:25 -0700

    Forms the 24bit integer?

--- octal_method_test.cpp
+++ octal_method_test.cpp
@@ -9,11 +9,15 @@ encoding_same_as_octal_method() {
 
 	char input[4] = "";
 	unsigned char output[4];
+	unsigned int twenty_fourbit;
 
 	for (int i = 0; i < 50; i++) {
 		input[0] = dist(rand);
 		input[1] = dist(rand);
 		input[2] = dist(rand);
+		twenty_fourbit = (unsigned int)input[0] << 16
+			| (unsigned int)input[1] << 8
+			| (unsigned int)input[0];
 	}
 }
 
