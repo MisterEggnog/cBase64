3bd1e72a0cbb72c2f7c87c467e1df1fef5e2ad30
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2022-11-10 15:49:37 -0800

    Made encode function safely handl smallr input siz

--- base64.c
+++ base64.c
@@ -19,16 +19,19 @@ static inline unsigned char fourth_sextet(const unsigned char* input) {
 
 void
 base64_encode(const unsigned char input[], size_t len, char dest[]) {
+	unsigned char local_input[3] = { input[0] };
 	dest[3] = dest[2] = PADDING;
 
 	switch (len) {
 		case 3:
-			dest[3] = BASE64_DIGITS[fourth_sextet(input)];
+			local_input[2] = input[2];
+			dest[3] = BASE64_DIGITS[fourth_sextet(local_input)];
 		case 2:
-			dest[2] = BASE64_DIGITS[third_sextet(input)];
+			local_input[1] = input[1];
+			dest[2] = BASE64_DIGITS[third_sextet(local_input)];
 		case 1:
-			dest[1] = BASE64_DIGITS[second_sextet(input)];
-			dest[0] = BASE64_DIGITS[first_sextet(input)];
+			dest[1] = BASE64_DIGITS[second_sextet(local_input)];
+			dest[0] = BASE64_DIGITS[first_sextet(local_input)];
 			break;
 		default:
 			break;
