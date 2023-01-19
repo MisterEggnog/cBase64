fa9569fb2b8c27c2ca0e22c86afc6abaa2287482
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-01-19 15:01:06 -0800

    Removed local array

--- base64.c
+++ base64.c
@@ -39,19 +39,16 @@ static inline unsigned char fourth_sextet(const unsigned char* input) {
 
 void
 base64_encode(const unsigned char input[], size_t len, char dest[]) {
-	unsigned char local_input[3] = { input[0] };
 	dest[3] = dest[2] = PADDING;
 
 	switch (len) {
 		case 3:
-			local_input[2] = input[2];
-			dest[3] = BASE64_DIGITS[fourth_sextet(local_input)];
+			dest[3] = BASE64_DIGITS[fourth_sextet(input)];
 		case 2:
-			local_input[1] = input[1];
-			dest[2] = BASE64_DIGITS[third_sextet(local_input)];
+			dest[2] = BASE64_DIGITS[third_sextet(input)];
 		case 1:
-			dest[1] = BASE64_DIGITS[second_sextet(local_input)];
-			dest[0] = BASE64_DIGITS[first_sextet(local_input)];
+			dest[1] = BASE64_DIGITS[second_sextet(input)];
+			dest[0] = BASE64_DIGITS[first_sextet(input)];
 			break;
 		default:
 			break;
