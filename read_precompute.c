b2aaf9701cf490e43e8135598ead3c7c63ec52d9
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-01-21 10:03:37 -0800

    Wrote function signature for b64 reader

--- read_precompute.c
+++ read_precompute.c
@@ -1,4 +1,14 @@
 #include <acutest.h>
+#include <stdio.h>
+
+struct base64_precompute {
+	unsigned char raw[3];
+	char three_length[4];
+	char two_length[4];
+	char one_length[4];
+};
+
+void read_base64_data(FILE* source, struct base64_precompute* dest);
 
 TEST_LIST = {
 	{ NULL, NULL },
