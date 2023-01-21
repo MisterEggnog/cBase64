4cdf0134e20d0c12b24c95835cae00800f9e5198
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-01-21 11:50:31 -0800

    open temp file for testing

--- read_precompute.c
+++ read_precompute.c
@@ -8,8 +8,23 @@ struct base64_precompute {
 	char one_length[4];
 };
 
-void read_base64_data(FILE* source, struct base64_precompute* dest);
+void read_base64_data(FILE* source, struct base64_precompute* dest) {
+}
+
+
+void
+read_into_table_test(void) {
+	FILE* temp_file = tmpfile();
+	fprintf(temp_file, "ABCDEF|abcd|abc=|ab==\n");
+	rewind(temp_file);
+
+	struct base64_precompute result;
+	read_base64_data(temp_file, &result);
+
+	fclose(temp_file);
+}
 
 TEST_LIST = {
+	{ "read_into_table_test", read_into_table_test },
 	{ NULL, NULL },
 };
