ace75df0724f61df739b343f91b84e24f575f4aa
Josiah Baldwin <jbaldwin@nwvariancesoftware.dev>
2023-01-20 17:11:26 -0800

    Finished python base64 generator

--- generate_data.py
+++ generate_data.py
@@ -4,7 +4,6 @@ from base64 import standard_b64encode
 from typing import Tuple
 
 def generate_b64() -> Tuple[int, str, str, str]:
-	print(2**24 - 1)
 	data = randint(0, 2**24 - 1)
 	data_bytes = bytearray(data.to_bytes(3, byteorder='big'))
 	
@@ -17,9 +16,9 @@ def generate_b64() -> Tuple[int, str, str, str]:
 	return (data, str(three_bytes, 'utf-8'), str(two_bytes, 'utf-8'), str(one_bytes, 'utf-8'))
 
 def b64_print(tup):
-	print("{:03X}|{}|{}|{}".format(tup[0], tup[1], tup[2], tup[3]))
+	print("{:06X}|{}|{}|{}".format(tup[0], tup[1], tup[2], tup[3]))
 
 seed(6)
-b64 = generate_b64()
-print(b64)
-b64_print(b64)
+for _ in range(0, 50):
+	b64 = generate_b64()
+	b64_print(b64)
